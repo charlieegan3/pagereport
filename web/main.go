acb49e1b831d4a59f5e226db80a198c127c8eef2
Charlie Egan <git@charlieegan3.com>
2016-01-16 19:16:44 +0000

    Implement basic server with amber and pg
    Motivation:
    -preparation for setting up a simple site to create jobs
    
    Change Explanation
    -setup amber templating to render content in a base template using a
    slim-like syntax.
    -setup pg connection string in docker-compose and connect to database as
    part of the application init

--- web/main.go
+++ web/main.go
@@ -1,24 +1,56 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"html/template"
+	"io"
+	"net/http"
 	"os"
 
 	"github.com/azer/crud"
+	"github.com/eknkc/amber"
 	_ "github.com/lib/pq"
 )
 
 var DB *crud.DB
+var compiler *amber.Compiler
+
+func init() {
+	DB, _ := crud.Connect("postgres", os.Getenv("DATABASE_URL"))
+	DB.Ping()
+	compiler = amber.New()
+}
+
+func renderError(w http.ResponseWriter, err error) {
+	io.WriteString(w, fmt.Sprintf("%v", err))
+}
+
+func renderPage(w http.ResponseWriter, renderedTemplate string) error {
+	data := struct{ Body template.HTML }{template.HTML(renderedTemplate)}
+	t, err := template.ParseFiles("templates/base.html")
+	t.Execute(w, data)
+	return err
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	err := compiler.ParseFile("templates/index.amber")
+	if err != nil {
+		renderError(w, err)
+	}
+	tpl, err := compiler.Compile()
+	if err != nil {
+		renderError(w, err)
+	}
+	templateBuffer := new(bytes.Buffer)
+	tpl.Execute(templateBuffer, nil)
+	err = renderPage(w, templateBuffer.String())
+	if err != nil {
+		renderError(w, err)
+	}
+}
 
 func main() {
-	DB, err := crud.Connect("postgres", os.Getenv("DATABASE_URL"))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	err = DB.Ping()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
